a5a98aa5b866bbea144f99e3c66ddb89fa266039
Mauve3 <mauvemoonman@gmail.com>
2017-03-16 15:52:49 -0700

    hopefully this fixes things

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,5 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
-import { browserHistory } from 'react-router'
 
 class App extends Component {
   static propTypes = {
