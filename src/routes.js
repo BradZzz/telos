bb80e1698c99ed11069e784637954af948f7f9d7
Mauve3 <mauvemoonman@gmail.com>
2017-03-16 03:01:33 -0700

    almost done...

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, browserHistory } from 'react-router'
 import AppSplash from './containers/AppSplash'
 import AppDemo from './containers/AppDemo'
 
-export default <Router history={hashHistory}>
+export default <Router history={browserHistory}>
                    <Route path="/" component={AppSplash} />
                    <Route path="/demo" component={AppDemo} />
                 </Router>
