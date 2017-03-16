3b6af429c1c1c0d440b5280112a97b6f4875ea18
Mauve3 <mauvemoonman@gmail.com>
2017-03-16 16:07:13 -0700

    added production webpack

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, Route, browserHistory, Redirect, IndexRoute } from 'react-router'
+import { Router, Route, hashHistory, Redirect, IndexRoute } from 'react-router'
 import App from './containers/App'
 import AppSplash from './containers/AppSplash'
 import AppDemo from './containers/AppDemo'
@@ -15,7 +15,7 @@ import AppDemo from './containers/AppDemo'
 //   <Route path="demo" component={AppDemo} />
 //</Route>
 
-export default <Router history={browserHistory}>
+export default <Router history={hashHistory}>
   <Route path="/" component={App}>
     <IndexRoute component={AppSplash}/>
     <Route path="demo" component={AppDemo}/>
